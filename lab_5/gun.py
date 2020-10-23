65202055599681722c8d0def2006e936f4c02caf
d-khromov <khromov.da@phystech.edu>
2020-10-23 12:09:17 +0300

    Add gravity and garbage collector.

--- lab_5/gun.py
+++ lab_5/gun.py
@@ -8,9 +8,6 @@ SCREEN_SIZE = [800, 600]
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
-TIME_STEP = 0.01
-GRAVITY = 10
-
 
 class Ball:
     def __init__(self, coord, vel, rad=15, color=None):
@@ -20,14 +17,19 @@ class Ball:
         self.coord = coord
         self.vel = vel
         self.rad = rad
+        self.is_alive = True
 
     def draw(self, screen):
         pg.draw.circle(screen, self.color, self.coord, self.rad)
 
-    def move(self, t_step=1.):
+    def move(self, t_step=1., g=2.):
+        self.vel[1] += int(g * t_step)
         for i in range(2):
             self.coord[i] += int(self.vel[i] * t_step)
         self.check_walls()
+        if self.vel[0]**2 + self.vel[1]**2 < 2**2 and\
+                self.coord[1] > SCREEN_SIZE[1] - 2*self.rad:
+            self.is_alive = False
 
     def check_walls(self):
         n = [[1, 0], [0, 1]]
@@ -39,7 +41,7 @@ class Ball:
                 self.coord[i] = SCREEN_SIZE[i] - self.rad
                 self.flip_vel(n[i])
 
-    def flip_vel(self, axis, coef_perp=1., coef_par=1.):
+    def flip_vel(self, axis, coef_perp=0.8, coef_par=0.9):
         vel = np.array(self.vel)
         n = np.array(axis)
         n = n / np.linalg.norm(n)
@@ -47,10 +49,6 @@ class Ball:
         vel_par = vel - vel_perp
         ans = -vel_perp * coef_perp + vel_par * coef_par
         self.vel = ans.astype(np.int).tolist()
-
-
-class Target:
-    pass
 
 
 class Gun:
@@ -83,6 +81,10 @@ class Gun:
                                 mouse_pos[0] - self.coords[0])
 
 
+class Target:
+    pass
+
+
 class ScoreTable:
     pass
 
@@ -96,6 +98,7 @@ class Manager:
     def process(self, events, screen):
         done = self.handle_events(events)
 
+        self.check_alive()
         self.move()
         self.draw(screen)
 
@@ -111,6 +114,14 @@ class Manager:
         for ball in self.balls:
             ball.move()
         self.gun.gain_power()
+
+    def check_alive(self):
+        dead_balls = []
+        for i, ball in enumerate(self.balls):
+            if not ball.is_alive:
+                dead_balls.append(i)
+        for i in reversed(dead_balls):
+            self.balls.pop(i)
 
     def handle_events(self, events):
         done = False
